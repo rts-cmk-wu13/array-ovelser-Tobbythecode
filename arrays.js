de6650011bcd9062aaad48200396d70497530c0d
Tobias <tobimation23@gmail.com>
2025-02-03 09:05:13 +0100

    opgave 1

--- arrays.js
+++ arrays.js
@@ -2,8 +2,24 @@ document.addEventListener("DOMContentLoaded", function() {
     let catsArray = ["Ollie", "Sophie", "Salem", "Tiger", "Binx", "Pumpkin", "Penny", "Lenny"];
     let dogsArray = ["Barney", "Molly", "Baxter", "Polly", "Buddy", "Suki", "Watson"];
     let fruitsArray = ["Banana", "Orange", "Pineapple", "Mango", "Plum", "Apricot"]
-    let numbersArray = [12, 4, 45, 17, 2, 26, 7, 55, 48, 32, 21]
+    let numbersArray = [12, 4, 45, 17, 2, 26, 7, 55, 48, 32, 21];
+ 
 
     // --- Løs opgaverne herunder --- 
+
+
+    // Opgave 1
+
+// let isbelow = (currentValue) => currentValue < 20;
+
+// console.log(numbersArray.every(isbelow));
+
+const result = numbersArray.filter(checknumberval);
+
+function checknumberval(number){
+    return number >= 20;
+}
+
+console.log(result);
 
 }) // ends DOMContentLoaded
